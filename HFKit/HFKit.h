3aa0e4a95691b5cb51dae93f49f0900ca613966f
salimbraksa <salim.braksa@gmail.com>
2018-02-23 22:44:20 +0000

    Initial Commit

---
+++ HFKit/HFKit.h
@@ -0,0 +1,19 @@
+//
+//  HFKit.h
+//  HFKit
+//
+//  Created by Salim Braksa on 2/23/18.
+//  Copyright © 2018 Hidden Founders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFKit.
+FOUNDATION_EXPORT double HFKitVersionNumber;
+
+//! Project version string for HFKit.
+FOUNDATION_EXPORT const unsigned char HFKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFKit/PublicHeader.h>
+
+
