48d3fbaf5c2360507c60595df780b73140f9705f
salimbraksa <salim.braksa@gmail.com>
2018-02-28 15:32:20 +0000

    Initialize UIColor from hex

--- HFKit/Extensions/UIColorExtensions.swift
+++ HFKit/Extensions/UIColorExtensions.swift
@@ -25,4 +25,19 @@ public extension UIColor {
         
     }
     
+    // MARK: - Initializers
+    
+    /**
+     The six-digit hexadecimal representation of color of the form #RRGGBB.
+     
+     - parameter hex6: Six-digit hexadecimal value.
+     */
+    public convenience init(hex: UInt32, alpha: CGFloat = 1) {
+        let divisor = CGFloat(255)
+        let red     = CGFloat((hex & 0xFF0000) >> 16) / divisor
+        let green   = CGFloat((hex & 0x00FF00) >>  8) / divisor
+        let blue    = CGFloat( hex & 0x0000FF       ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
 }
